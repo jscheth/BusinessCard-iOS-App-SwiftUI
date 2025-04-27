0297e2322c602ebc849e0e1719f800456e44aa04
jscheth <sovichea.chet@gmail.com>
2025-04-27 13:11:00 -0700

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusinessCardTests/BusinessCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardTests.swift
+//  BusinessCardTests
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import Testing
+@testable import BusinessCard
+
+struct BusinessCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
