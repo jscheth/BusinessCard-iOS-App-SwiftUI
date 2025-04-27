1fa2a2ca1ef6f11e1a16856b649609fced69f861
jscheth <sovichea.chet@gmail.com>
2025-04-27 13:40:43 -0700

    Init Commit

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.06, green: 0.74, blue: 0.98, opacity: 1.00)
+                .ignoresSafeArea()
+            VStack {
+                Text("Jonathan Cheth")
+                    .bold()
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                Text("iOS & Web Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
     }
 }
 
